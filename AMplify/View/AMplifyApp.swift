402d255e6a5adf0a264e9877381219a766b85cfe
ZyVoir <whcs.william@gmail.com>
2025-03-23 22:22:28 +0700

    Initial Commit

---
+++ AMplify/View/AMplifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AMplifyApp.swift
+//  AMplify
+//
+//  Created by William on 23/03/25.
+//
+
+import SwiftUI
+
+@main
+struct AMplifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
