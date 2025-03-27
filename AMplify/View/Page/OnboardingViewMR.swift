07bd0be2c9db1bd9e1ce9f5e9406c2f3ff4291ac
Priscilla <kurniawanpriscilla@gmail.com>
2025-03-27 08:59:12 +0700

    27/03/2025 - OnboardingViewAlarm - Added new page (alarm, ListView UI)

---
+++ AMplify/View/Page/OnboardingViewMR.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingViewMR.swift
+//  AMplify
+//
+//  Created by Priscilla Anthonio on 26/03/25.
+//
+
+import SwiftUI
+
+struct OnboardingViewMR: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingViewMR()
+}
