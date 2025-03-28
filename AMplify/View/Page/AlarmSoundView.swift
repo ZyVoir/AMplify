7daa7694c7edb8e324f8d155e819018ae26d1383
Priscilla <kurniawanpriscilla@gmail.com>
2025-03-28 09:53:05 +0700

    28/03/2025 - AlarmSoundView (Alarm Sound List with Radio Button UI)

--- AMplify/View/Page/AlarmSoundView.swift
+++ AMplify/View/Page/AlarmSoundView.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct AlarmSoundView: View {
+    
+    @State private var selectedSound: String = "Radar"
+    let alarmSounds = ["Radar", "Beacon", "Chimes", "Circuit", "Reflection", "Waves"]
+
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            List{
+                ForEach(alarmSounds, id: \.self){ sound in
+                    HStack{
+                        Text(sound)
+                        Spacer()
+                        RadioButton(isSelected: selectedSound == sound)
+                    }
+                    .contentShape(Rectangle())
+                    .onTapGesture{
+                        selectedSound = sound
+                    }
+                }
+            }
+            .navigationTitle("Alarm Sound")
+        }
+        
+        .scrollContentBackground(.hidden)
+    }
+}
+
+struct RadioButton: View {
+    var isSelected: Bool
+    
+    var body: some View {
+        Image(systemName: isSelected ? "largecircle.fill.circle" : "circle")
+            .foregroundStyle(isSelected ? Color.blue : Color.gray)
     }
 }
 
