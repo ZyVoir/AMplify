d849b945ec8250792e552beb761a87be479f6c4f
Priscilla <kurniawanpriscilla@gmail.com>
2025-03-28 10:52:03 +0700

    28/03/2025 - OnboardingViewMR, TimerSoundView (added navigation to list & added TimerSoundView UI)

---
+++ AMplify/View/Page/TimerSoundView.swift
@@ -0,0 +1,40 @@
+//
+//  TimerSoundView.swift
+//  AMplify
+//
+//  Created by Priscilla Anthonio on 28/03/25.
+//
+
+import SwiftUI
+
+struct TimerSoundView: View {
+    
+    @State private var selectedSound: String = "Radar"
+    let alarmSounds = ["Radar", "Beacon", "Chimes", "Circuit", "Reflection", "Waves"]
+    
+    
+    var body: some View {
+        NavigationStack{
+            List{
+                ForEach(alarmSounds, id: \.self){ sound in
+                    HStack{
+                        Text(sound)
+                        Spacer()
+                        RadioButton(isSelected: selectedSound == sound)
+                    }
+                    .contentShape(Rectangle())
+                    .onTapGesture{
+                        selectedSound = sound
+                    }
+                }
+            }
+            .navigationTitle("Timer Sound")
+        }
+        
+        .scrollContentBackground(.hidden)
+    }
+}
+
+#Preview {
+    TimerSoundView()
+}
